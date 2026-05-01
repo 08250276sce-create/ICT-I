7d6ae96fc9ccecbee1e6ddb61f5e51c80c65a2c8
ICT1 <08250276.sce@rub.edu.bt>
2026-05-01 15:26:49 +0600

    .\dict.py

---
+++ Demo/dict.py
@@ -0,0 +1,19 @@
+userDetails={'Id':1,'username':'Just_me'}
+print(type(userDetails))
+print(userDetails)
+location=dict(s='Samtse',t='Thimphu',p='Paro')
+print(location)
+print(userDetails['username'])
+print(location.get('t'))
+userDetails['email']='justme@example.com'
+print(userDetails)
+userDetails['username']='just_me_updated'
+print(userDetails)
+del location['p']
+print(location)
+deleted_value=userDetails.pop('email')
+print(deleted_value)
+del_key,del_value=userDetails.popitem()
+print(f'the deleted key is{del_key}and the deleted value is{del_value}')
+location.clear()
+print(location)

5709e7809e218a6ef0c1e0f838d33544b060a7fb
ICT1 <08250276.sce@rub.edu.bt>
2026-05-01 15:31:21 +0600

    .\labtask.py

---
+++ Demo/labtask.py
@@ -0,0 +1,15 @@
+students_list=[]
+student_dict={}
+
+students_list.append("Namdrel Zangpo")
+students_dict["Namdrel Zangpo"]="age=24" and "Grade 12"
+
+students_list.append("Ugyen dorji")
+students_dict["Ugyen dorji"]="age=20" and "Grade 10"
+
+students_list.append("Kelzang Dechen")
+students_dict["Kelzang Dechen"]="age=19" and "Grade 11"
+
+students_list.append("Choki penjor")
+students_dict["Choki penjor"]="age=29" and "Grade 12"
+
